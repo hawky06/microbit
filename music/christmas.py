65aa9fa90dc46d2a01fb50d9195cc5d6f2448bde
Alex Hawkins <athaug06@gmail.com>
2019-12-13 19:14:45 +0000

    music

--- music/christmas.py
+++ music/christmas.py
@@ -16,11 +16,14 @@ jingle = [
  'e:2','e:2','e:4','e:2','e:2','e:4','e:2','g:2','c','d','e:8','f:2','f:2','f:2',
  'f:2','f:2','e:2','e:2','e:1','e:1','g:2','g:2','f:2','d:2', 'c:4'
 ]
-
+number=1
 while True:
-    if button_a.is_pressed():
-        music.play(jingle,wait=False,loop=False)
-        display.scroll("jingle bells",wait=False,loop=False)
-    elif button_b.is_pressed():
+    if button_a.was_pressed():
+        display.show(number)
+        number+=1
+        if number>3:
+            number=1
+            
+    elif button_b.was_pressed():
         music.play(wewishyou,wait=False,loop=False)
         display.scroll("we wish you a merry christmas",wait=False,loop=False)
